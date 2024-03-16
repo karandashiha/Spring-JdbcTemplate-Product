6af4adf073d03e79528effdb34e08776abb6234d
Katy Reshetnyk <kreshetnyk@gmail.com>
2024-03-16 22:13:24 +0200

    Initial commit

---
+++ src/main/java/org/example/app/conf/AppConfig.java
@@ -0,0 +1,31 @@
+package org.example.app.conf;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+
+@Configuration
+@ComponentScan("org.example.app")
+@PropertySource("classpath:db.properties")
+public class AppConfig {
+
+    @Autowired
+    Environment environment;
+
+    @Bean
+    DataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setUrl(environment.getProperty("dbUrl"));
+        driverManagerDataSource.setUsername(environment.getProperty("dbUserName"));
+        driverManagerDataSource.setPassword(environment.getProperty("dbPassword"));
+        driverManagerDataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("dbDriver")));
+        return driverManagerDataSource;
+    }
+}
---
+++ src/main/java/org/example/app/entity/Product.java
@@ -0,0 +1,93 @@
+package org.example.app.entity;
+
+import org.example.app.utils.Rounder;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class Product {
+
+    private Long id;
+    private String name;
+    private int quota;
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(String name, int quota, double price) {
+        this.name = name;
+        this.quota = quota;
+        this.price = price;
+    }
+
+    public Product(Long id, String name, int quota, double price) {
+        this.id = id;
+        this.name = name;
+        this.quota = quota;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (!Objects.equals(id, product.id)) return false;
+        if (!Objects.equals(name, product.name)) return false;
+        if (!Objects.equals(quota, product.quota)) return false;
+        return Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + quota;
+        result = (int) (31 * result + price);
+        return result;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuota() {
+        return quota;
+    }
+
+    public void setQuota(int quota) {
+        this.quota = quota;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "id -> " + this.getId() +
+                ", product name -> " + this.getName() +
+                ", quota -> " + this.getQuota() +
+                ", price -> " + Rounder.round(this.getPrice(),2) + "\n";
+    }
+}
